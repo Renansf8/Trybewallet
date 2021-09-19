39f96f1cf8411c06495dde0938fc6740ace316c1
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-19 11:28:32 -0300

    Updates

---
+++ precache-manifest.40426dd283460c7be874d0df1b75caa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfdff3a8b78a399c4e1bf313b4d5b82",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "8c443b86216be4bc2785",
+    "url": "/trybewallet/static/css/main.04c58498.chunk.css"
+  },
+  {
+    "revision": "6f5ab7f33256f8d50e3f",
+    "url": "/trybewallet/static/js/2.1bb02932.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.1bb02932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c443b86216be4bc2785",
+    "url": "/trybewallet/static/js/main.4d9c860e.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  },
+  {
+    "revision": "ed450dd4de3c07cf50b840cc473d3e9e",
+    "url": "/trybewallet/static/media/wallet3.ed450dd4.png"
+  }
+]);
